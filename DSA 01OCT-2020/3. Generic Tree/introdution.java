0882a72645eb50b6975b5c606e796186e8dab94c
Mohit Behl <mohitbehlofficial@gmail.com>
2021-01-02 13:57:39 +0530

    Update introdution.java

--- DSA 01OCT-2020/3. Generic Tree/introdution.java
+++ DSA 01OCT-2020/3. Generic Tree/introdution.java
@@ -213,6 +213,119 @@ public class Inroduction {
         System.out.println(pre + "\n" + post);
     }
 
+
+    public static class MSNode {
+        int ht, sum, max, min, size;
+        MSNode() {
+            ht = -1;
+            sum = 0;
+            max = Integer.MIN_VALUE;
+            min = Integer.MAX_VALUE;
+            size = 0;
+        }
+        MSNode(int ht, int sum, int max, int min, int size) {
+            this.ht = ht;
+            this.sum = sum;
+            this.max = max;
+            this.min = min;
+            this.size = size;
+        }
+    }
+    public static MSNode multisolver(Node node) {
+        int ht = -1, sum = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, size = 0;
+
+        for (Node child: node.children) {
+            MSNode resChild = multisolver(child);
+
+            ht = Math.max(ht, resChild.ht);
+            sum += resChild.sum;
+            max = Math.max(max, resChild.max);
+            min = Math.min(min, resChild.min);
+            size += resChild.size;
+        }
+
+        ht += 1;
+        sum += node.data;
+        max = Math.max(max, node.data);
+        min = Math.min(min, node.data);
+        size += 1;
+
+        MSNode myans = new MSNode(ht, sum, max, min, size);
+        return myans;
+    }
+
+    public static boolean areMirror(Node n1, Node n2) {
+        if (n1.children.size() != n2.children.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < n1.children.size(); i++) {
+            Node child1 = n1.children.get(i); // l -> r
+            Node child2 = n2.children.get(n2.children.size() - 1 - i); // r -> l
+
+            if (areMirror(child1, child2) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean IsSymmetric(Node node) {
+        return areMirror(node, node);
+    }
+
+    static Node predecessor;
+    static Node successor;
+    static int state;
+    public static void predecessorAndSuccessor(Node node, int data) {
+        if (state == 0) {
+            if (node.data == data) {
+                // element found
+                state = 1;
+            } else {
+                predecessor = node;
+            }
+        } else if (state == 1) {
+            successor = node;
+            state = 2;
+        }
+
+        for (Node child: node.children) {
+            predecessorAndSuccessor(child, data);
+        }
+    }
+
+    public static int kthLargest(Node node, int k) {
+        int data = Integer.MAX_VALUE;
+        floor = Integer.MIN_VALUE;
+        for (int i = 1; i <= k; i++) {
+            ceilAndFloor(node, data);
+            data = floor;
+            floor = Integer.MIN_VALUE;
+        }
+        return data;
+    }
+
+    public static void removeLeaves(Node node) {
+        // remove all children having no further child --> leaf nodes
+        for (int idx = node.children.size() - 1; idx >= 0; idx--) {
+            // child 
+            Node child = node.children.get(idx);
+
+            if (child.children.size() == 0) {
+                // child --> leaf node
+                node.children.remove(idx);
+            }
+        }
+
+        // leaf nodes have been removed , move to subtree
+        for (Node child: node.children) {
+            removeLeaves(child);
+        }
+
+    }
+
     public static void main(String[] args) {
 
         int input[] = {
@@ -231,5 +344,11 @@ public class Inroduction {
         };
         Node root = construct(input);
         display(root);
+        MSNode ans = multisolver(root);
+        System.out.println("ht : " + ans.ht);
+        System.out.println("sum : " + ans.sum);
+        System.out.println("max : " + ans.max);
+        System.out.println("min : " + ans.min);
+        System.out.println("size : " + ans.size);
     }
 }
