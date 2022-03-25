f67dbefbfc18c602c33087c5a53a7472b2c449af
MohitBehl <mohitbehlofficial@gmail.com>
2022-03-25 23:37:39 +0530

    update

--- FJP6/2d Array/MatrixMultiplication.java
+++ FJP6/2d Array/MatrixMultiplication.java
@@ -18,8 +18,28 @@ public class Main{
       }
     }
     public static int[][] matrixMultiplication(int mat1[][],int mat2[][]){
-        int res[][] = new int[][];
-        // main logic
+        int nr1 = mat1.length , nc1 = mat1[0].length;
+        int nr2 = mat2.length , nc2 = mat2[0].length;
+        
+        int res[][] = null;
+        if(nc1 == nr2){
+            // matrix multiplication is possible
+            res = new int[nr1][nc2];
+
+            for(int r = 0 ; r < res.length ; r++){
+                for(int c = 0 ; c < res[0].length ; c++){
+
+                    for(int k = 0 ; k < nc1 ; k++){
+                        res[r][c] = res[r][c] + (mat1[r][k] * mat2[k][c]);
+                    }
+
+                }
+            }
+
+            return res;
+        }else{
+            System.out.println("Invalid input");
+        }
         return res;
     }
     public static void main(String[] args){
@@ -35,7 +55,9 @@ public class Main{
         input(mat2,scn);
 
         int res[][] = matrixMultiplication(mat1,mat2);
-        display(res);
+        if(res != null){
+            display(res);
+        }
     }
 
 }
