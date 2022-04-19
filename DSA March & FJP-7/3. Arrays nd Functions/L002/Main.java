4deb55c77f746b83f0a3b725ec7e11446ac4f14e
MohitBehl <mohitbehlofficial@gmail.com>
2022-04-19 22:41:35 +0530

    Update Main.java

--- DSA March & FJP-7/3. Arrays nd Functions/L002/Main.java
+++ DSA March & FJP-7/3. Arrays nd Functions/L002/Main.java
@@ -3,16 +3,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
+        //  input
         int n = scn.nextInt();
         int arr[] = new int[n];
         for(int i = 0 ; i < n ; i++){
             arr[i] = scn.nextInt();
         }
         int k = scn.nextInt();
+
+        // rotate
+        rotate(arr, k);
+        // display
+        display(arr);
     }
 
     public static void rotate(int arr[],int k){
-        // code        
+        k = k % arr.length;
+
+        if(k < 0)
+            k = k + arr.length;
+        
+        // reverse part 2
+        reverse(arr, arr.length-k, arr.length-1);
+        // reverse part 1
+        reverse(arr, 0, arr.length-k-1);
+        // reverse arr
+        reverse(arr, 0, arr.length-1);
+    }
+    public static void reverse(int arr[],int left,int right){
+        while(left < right){
+            int tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+    public static void display(int arr[]){
+        for(int vl : arr){
+            System.out.print(vl+" ");
+        }
     }
 }
