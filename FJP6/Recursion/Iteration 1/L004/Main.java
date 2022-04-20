2967358e09991f49183ae9bfabeabbf36bc45fab
MohitBehl <mohitbehlofficial@gmail.com>
2022-04-20 23:35:36 +0530

    Update Main.java

--- FJP6/Recursion/Iteration 1/L004/Main.java
+++ FJP6/Recursion/Iteration 1/L004/Main.java
@@ -33,4 +33,36 @@ public class Main {
 
         return myList;
     }
+
+    public static ArrayList<String> getStairPath(int n){
+        if(n == 0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        ArrayList<String> myList = new ArrayList<>();
+        if(n >= 1){ // 1 len 
+            ArrayList<String> paths = getStairPath(n-1);
+            for(String path : paths){
+                myList.add(1+path);
+            }
+        }
+
+        if(n >= 2){ // 2 len 
+            ArrayList<String> paths = getStairPath(n-2);
+            for(String path : paths){
+                myList.add(2+path);
+            }
+        }
+
+        if(n >= 3){ // 3 len 
+            ArrayList<String> paths = getStairPath(n-3);
+            for(String path : paths){
+                myList.add(3+path);
+            }
+        }
+
+        return myList;
+    }
 }
