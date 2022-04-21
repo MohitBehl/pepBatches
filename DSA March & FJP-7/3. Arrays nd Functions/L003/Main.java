fe27fb7deaa3edd0bc3521357df9dc069916fd4d
MohitBehl <mohitbehlofficial@gmail.com>
2022-04-21 23:04:16 +0530

    Create Main.java

---
+++ DSA March & FJP-7/3. Arrays nd Functions/L003/Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean binarySearch(int arr[],int ele){ // increasing Order
+        int left = 0 , right = arr.length-1;
+
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(ele == arr[mid]){
+                return true;
+            }else if(ele > arr[mid]){ // move right
+                left = mid + 1;
+            }else if(ele < arr[mid]){
+                right = mid - 1;
+            }
+        }
+        return false;
+    }  
+    
+    // arr : 1 2 2 2 3 4 , k = 2 
+    public static int firstIdx(int arr[] ,int k){
+        int left = 0 , right = arr.length-1;
+        int idx = -1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(arr[mid] == k){
+                idx = mid;
+                right = mid-1;
+            }else if(k > arr[mid]){
+                left = mid+1;
+            }else if(k < arr[mid]){
+                right = mid-1;
+            }
+        }
+
+        return idx;
+    }
+
+    public static int lastIdx(int arr[] ,int k){
+        int left = 0 , right = arr.length-1;
+        int idx = -1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(arr[mid] == k){
+                idx = mid;
+                left = mid + 1;
+            }else if(k > arr[mid]){
+                left = mid+1;
+            }else if(k < arr[mid]){
+                right = mid-1;
+            }
+        }
+
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = scn.nextInt();
+        }
+        int ele = scn.nextInt();
+
+        // boolean res = binarySearch(arr, ele);
+
+        System.out.println(firstIdx(arr, k));
+        System.out.println(lastIdx(arr, k));
+    }
+}
