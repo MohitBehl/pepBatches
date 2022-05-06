1940377db5d220b5e4770b0e4800aca854f6f294
MohitBehl <mohitbehlofficial@gmail.com>
2022-05-06 23:40:37 +0530

    Update Main.java

--- FJP6/Recursion/Iteration 1/L008/Main.java
+++ FJP6/Recursion/Iteration 1/L008/Main.java
@@ -4,16 +4,23 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        // Scanner scn = new Scanner(System.in);
+        // int n = scn.nextInt();
+        // int m = scn.nextInt();
+        // int[][] arr = new int[n][m];
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         arr[i][j] = scn.nextInt();
+        //     }
+        // }
+        // floodfill(arr, 0, 0, "",new boolean[n][m]);
+        
+
+        // N Queens
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int m = scn.nextInt();
-        int[][] arr = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = scn.nextInt();
-            }
-        }
-        floodfill(arr, 0, 0, "",new boolean[n][m]);
+        
+        printNQueens(new int[n][n],"",0);
     }
     
     // asf -> answer so far
@@ -26,11 +33,48 @@ public class Main {
             return;
         }
 
-        visited[sr][sc] = true; // mark 
+        visited[sr][sc] = true; // mark
+         
         floodfill(maze,sr-1,sc,asf+"t",visited);
         floodfill(maze,sr,sc-1,asf+"l",visited);
         floodfill(maze,sr+1,sc,asf+"d",visited);
         floodfill(maze,sr,sc+1,asf+"r",visited);
         visited[sr][sc] = false; /// unmark
+    }
+
+    public static boolean isSafe(int chess[][],int row,int col){
+        for(int i = row-1,j = col ; i >= 0 ; i--){ //  upwards
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
+
+        for(int i = row-1,j = col-1 ; i >= 0 && j >= 0 ; i--,j--){ // upper left diag
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
+
+        for(int i = row-1,j = col+1 ; i >= 0 && j < chess[0].length ; i--,j++){ // upper right diag
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public static void printNQueens(int[][] chess, String asf, int row) {
+        if(row == chess.length){
+            System.out.println(asf+".");
+            return;
+        }
+        for(int col = 0 ; col < chess[0].length ; col++){
+            if(isSafe(chess,row,col)){
+                chess[row][col] = 1;
+                printNQueens(chess,asf+row+"-"+col+", ",row+1);
+                chess[row][col] = 0;
+            }
+        }
     }
 }
