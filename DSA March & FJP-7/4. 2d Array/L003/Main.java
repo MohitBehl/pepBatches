972d47bc1e89e82c3784c839ad846a429966a43f
MohitBehl <mohitbehlofficial@gmail.com>
2022-05-07 22:52:59 +0530

    Update Main.java

--- DSA March & FJP-7/4. 2d Array/L003/Main.java
+++ DSA March & FJP-7/4. 2d Array/L003/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        // // spiral
+        // Scanner scn = new Scanner(System.in);
 
+        // int nr = scn.nextInt() , nc = scn.nextInt();
+        // int mat[][] = new int[nr][nc];
+        // for(int i = 0 ; i < nr ; i++){
+        //     for(int j = 0 ; j < nc ; j++){
+        //         mat[i][j] = scn.nextInt();
+        //     }
+        // }
+
+        // spiralDisplay(mat);
+
+        // // ring Rotate
+        Scanner scn = new Scanner(System.in);
         int nr = scn.nextInt() , nc = scn.nextInt();
         int mat[][] = new int[nr][nc];
         for(int i = 0 ; i < nr ; i++){
@@ -11,9 +24,59 @@ public class Main {
                 mat[i][j] = scn.nextInt();
             }
         }
+        int shell = scn.nextInt();
+        int k = scn.nextInt();
 
-        spiralDisplay(mat);
+        ringRotate(mat,shell,k);
+        display(mat);
     }
+
+    public static void ringRotate(int mat[][],int shell,int k){
+        int vals[] = getRing(mat,shell);
+        rotate(vals,k);
+        updateRing(mat,shell,vals);
+    }
+
+    public static int[] getRing(int mat[][],int shell){
+
+    }
+
+    public static void updateRing(int mat[][],int shell,int vals[]){
+
+    }
+
+    public static void rotate(int arr[],int k){
+        k = k % arr.length;
+
+        if(k < 0)
+            k = k + arr.length;
+        
+        // reverse part 2
+        reverse(arr, arr.length-k, arr.length-1);
+        // reverse part 1
+        reverse(arr, 0, arr.length-k-1);
+        // reverse arr
+        reverse(arr, 0, arr.length-1);
+    }
+    public static void reverse(int arr[],int left,int right){
+        while(left < right){
+            int tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    
     public static void spiralDisplay(int mat[][]){
         int rmin = 0, cmin = 0, rmax = mat.length-1, cmax = mat[0].length-1;
         int tne = mat.length * mat[0].length;
