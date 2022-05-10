fc37d94dec922fc7206805509a28c276505ed1ae
MohitBehl <mohitbehlofficial@gmail.com>
2022-05-10 23:10:08 +0530

    Create Main.java

---
+++ DSA March & FJP-7/4. Strings/L001/Main.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isPalindromic(String str){
+        int left = 0 , right = str.length()-1;
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+	public static void printAllPalindromicSubString(String str){
+        for(int sidx = 0 ; sidx < str.length() ; sidx++){
+            for(int eidx = sidx+1 ; eidx <= str.length() ; eidx++){
+                String ss = str.substring(sidx, eidx);
+                if(isPalindromic(ss))
+                    System.out.println(ss);
+            }
+        }
+	}
+
+
+    public static String compression1(String str){
+        String ans = ""+str.charAt(0);
+        int idx = 1;
+
+        while(idx < str.length()){
+            char curr = str.charAt(idx);
+            char prev = str.charAt(idx-1);
+
+            if(curr == prev){
+                // do nothing
+            }else{
+                ans = ans + curr;
+            }
+            idx++;
+        }
+		return ans;
+	}
+
+	public static String compression2(String str){
+        String ans = ""+str.charAt(0);
+        int idx = 1;
+        int count = 1;
+        while(idx < str.length()){
+            char curr = str.charAt(idx);
+            char prev = str.charAt(idx-1);
+
+            if(curr == prev){
+                count++;
+            }else{
+                if(count > 1){
+                    ans = ans + count;
+                }
+                ans = ans + curr;
+                count = 1;
+            }
+            idx++;
+        }
+        if(count > 1){
+            ans = ans + count;
+        }
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		// printAllPalindromicSubString(str);
+        System.out.println(compression1(str));
+        System.out.println(compression2(str));
+	}
+
+}
