6dc9c08e9bfd24cb4a1ce1f201ed514244f70d63
MohitBehl <mohitbehlofficial@gmail.com>
2022-05-19 22:16:12 +0530

    Create Main.java

---
+++ DSA March & FJP-7/5. Recursion/L002/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt(); 
+        int src = scn.nextInt();
+        int dest = scn.nextInt();
+        int helper = scn.nextInt();
+        toh(n,src,dest,helper);
+    }
+
+    public static void toh(int n, int src, int dest, int helper){
+        if(n == 0) return;
+        toh(n-1,src,helper,dest);
+        System.out.println(n+"["+src+" -> "+dest+"]");
+        toh(n-1,helper,dest,src);
+    }
+
+}
