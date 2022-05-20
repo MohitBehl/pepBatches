3c02fccad6a3934309ffc0923f88fbe5684852bc
MohitBehl <mohitbehlofficial@gmail.com>
2022-05-21 00:16:49 +0530

    code

--- DSA March & FJP-7/5. Recursion/L002/Main.java
+++ DSA March & FJP-7/5. Recursion/L002/Main.java
@@ -11,7 +11,20 @@ public class Main {
         int helper = scn.nextInt();
         toh(n,src,dest,helper);
     }
-
+    public static void pzz(int n){
+        if(n == 0) return;
+        if(n == 1) {System.out.print("111"); return;}
+        
+        System.out.print(n);
+        pzz(n-1);
+        System.out.print(n);
+        pzz(n-1);
+        System.out.print(n);
+    }
+    public static int fib(int n){
+        if(n == 0 || n == 1) return n;
+        return fib(n-1) + fib(n-2);
+    }
     public static void toh(int n, int src, int dest, int helper){
         if(n == 0) return;
         toh(n-1,src,helper,dest);
