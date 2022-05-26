5c258eccae01f37ce7b9ecb418efb33856adf726
MohitBehl <mohitbehlofficial@gmail.com>
2022-05-27 00:05:18 +0530

    Update Main.java

--- DSA March & FJP-7/5. Recursion/L004/Main.java
+++ DSA March & FJP-7/5. Recursion/L004/Main.java
@@ -34,4 +34,57 @@ public class Main {
         return ans;
     }
 
+    public static ArrayList<String> getStairPaths(int n) {
+        if(n == 0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+        ArrayList<String> myPath = new ArrayList<>();
+        if(n >= 1){
+            // 1 step 
+            ArrayList<String> path1Step = getStairPaths(n-1);
+            for(String path : path1Step){
+                myPath.add('1'+path);
+            }
+        }
+        if(n >= 2){
+            // 2 step 
+            ArrayList<String> path2Step = getStairPaths(n-2);
+            for(String path : path2Step){
+                myPath.add('2'+path);
+            }
+        }
+        if(n >= 3){
+            // 3 step 
+            ArrayList<String> path3Step = getStairPaths(n-3);
+            for(String path : path3Step){
+                myPath.add('3'+path);
+            }
+        }
+        return myPath;
+    }
+
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        if(sr > dr || sc > dc) { // invalid
+            ArrayList<String> base = new ArrayList<>();
+            return base;
+        }
+        if(sr == dr && sc == dc){ // valid
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+        ArrayList<String> hpath = getMazePaths(sr, sc+1, dr, dc);// h
+        ArrayList<String> vpath = getMazePaths(sr+1, sc, dr, dc);// v
+
+        ArrayList<String> path = new ArrayList<>();
+        for(String p : hpath){
+            path.add("h"+p);
+        }
+        for(String p : vpath){
+            path.add("v"+p);
+        }
+        return path;
+    }
 }
