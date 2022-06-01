e822d82d7b385139d603f5bcbcf2fbe2ae23f919
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-01 23:44:34 +0530

    Update Main.java

--- FJP6/LinkedList/L008/Main.java
+++ FJP6/LinkedList/L008/Main.java
@@ -54,3 +54,91 @@ public class Main{
         p1.next = head2;
     }
 }
+
+// segregate even odd
+/* 
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution{
+    Node divide(int N, Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node even = new Node(0);
+        Node odd = new Node(0);
+
+        Node evenTail = even , oddTail = odd;
+        Node curr = head;
+
+        while(curr != null){
+            if(curr.data % 2 == 0){
+                evenTail.next = curr;
+                evenTail = evenTail.next;
+            }else {
+                oddTail.next = curr;
+                oddTail = oddTail.next;
+            }
+            curr = curr.next;
+        }
+
+        oddTail.next = null;
+        evenTail.next = null;
+
+        evenTail.next = odd.next;
+
+        return even.next;
+    }
+
+
+}
+
+// rotate a linked list
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int size(ListNode head){
+        int count = 0;
+        while(head != null){
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null){ return head;}
+        int len = size(head);
+        k = k % len;
+        if(k == 0) return head;
+        
+        ListNode slow = head , fast = head;
+        while(k != 0){
+            fast = fast.next;
+            k--;
+        }
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        ListNode nbr = slow.next;
+        slow.next = null;
+        fast.next = head;
+        return nbr;
+    }
+}
