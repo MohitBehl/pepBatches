ee912284f96ad4fe599bbf174b808cc66906d518
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-11 23:52:15 +0530

    Update Main.java

--- DSA March & FJP-7/6. Linked List/L001/Main.java
+++ DSA March & FJP-7/6. Linked List/L001/Main.java
@@ -8,6 +8,7 @@ public class Main{
         }
         Node(int data){
             this.data = data;
+            this.next = null;
         }
         Node(int data,Node next){
             this.data = data;
@@ -23,9 +24,44 @@ public class Main{
             head = tail = null;
             size = 0;
         }
+
+        public void addFirst(int val) {
+            Node node = new Node(val);
+            if(this.size == 0){
+                this.head = this.tail = node;
+            }else{
+                node.next = this.head;
+                this.head = node;
+            }
+            this.size++;
+        }
+
+        public int size(){
+            return this.size;
+        }
+
+        public void display(){
+            if(this.size == 0) 
+                return;
+
+            Node curr = this.head;
+
+            while(curr != null){
+                System.out.print(curr.data+" ");
+                curr = curr.next;
+            }
+
+            System.out.println();
+        }
     }
     
     public static void main(){
         LinkedList ll = new LinkedList();
+        ll.addFirst(10); // 10
+        ll.addFirst(20); // 20 -> 10
+        ll.addFirst(30); // 30 -> 20 -> 10
+        System.out.println(ll.size());
+        ll.display();
     }
+
 }
