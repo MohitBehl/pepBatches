0222a30fd273b85981a11e2218f6bb5c6ae6984a
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-12 15:13:38 +0530

    Update Main.java

--- FJP6/Sorting/L002/Main.java
+++ FJP6/Sorting/L002/Main.java
@@ -40,6 +40,13 @@ public class Main{
     }
 
 
+    // used for swapping ith and jth elements of array
+    public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
     public static void partition(int[] arr, int pivot){
         int i = 0, j = 0;
 
@@ -55,11 +62,32 @@ public class Main{
         
     }
 
-    // used for swapping ith and jth elements of array
-    public static void swap(int[] arr, int i, int j) {
-        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    public static int getPivotIndexAfterPartition(int[] arr, int pivot , int lo,int hi){
+        int i = lo, j = lo;
+
+        while(j <= hi){
+            if(arr[j] <= pivot){
+                swap(arr,j,i);
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        
+        return i-1;
+    }
+
+   public static void quickSort(int arr[],int lo,int hi){
+        // if(lo > hi) return; // invalid range
+        // if(hi == lo) return; // because 1 ele array is always sorted
+
+        int pivot = arr[hi];
+        int pidx = getPivotIndexAfterPartition(arr,pivot,lo,hi);
+
+        if(lo < pidx-1)
+            quickSort(arr,lo,pidx-1);
+        if(pidx+1 < hi)
+            quickSort(arr,pidx+1,hi);
     }
 }
