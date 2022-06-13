ffeb855ac096b075e196001096faf01d55f73198
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-13 23:45:27 +0530

    Update Main.java

--- FJP6/Stacks & Queues/L002/Main.java
+++ FJP6/Stacks & Queues/L002/Main.java
@@ -63,4 +63,53 @@ public class Main{
         return val;
     }
   }
+  public static class QueueToStackAdapter {
+    Queue<Integer> mainQ;
+    Queue<Integer> helperQ;
+
+    public QueueToStackAdapter() {
+      mainQ = new ArrayDeque<>();
+      helperQ = new ArrayDeque<>();
+    }
+
+    int size() {
+        return mainQ.size();
+    }
+
+    void push(int val) {
+      mainQ.add(val);
+    }
+
+    int pop() {
+      if(mainQ.size() == 0){
+          System.out.println("Stack underflow");
+          return -1;
+      }
+      while(mainQ.size() > 1){
+        helperQ.add(mainQ.remove());
+      }
+      int val = mainQ.remove();
+      Queue<Integer> tmpQ = mainQ;
+      mainQ = helperQ;
+      helperQ = tmpQ;
+
+      return val;
+    }
+    int top() {
+      if(mainQ.size() == 0){
+          System.out.println("Stack underflow");
+          return -1;
+      }
+      while(mainQ.size() > 1){
+        helperQ.add(mainQ.remove());
+      }
+      int val = mainQ.remove();
+      helperQ.add(val);
+      Queue<Integer> tmpQ = mainQ;
+      mainQ = helperQ;
+      helperQ = tmpQ;
+
+      return val;
+    }
+  }
 }
