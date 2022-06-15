a48711216676d8b0536fae7420553f68555028dd
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-16 00:10:05 +0530

    code

--- FJP6/GT/L001/Main.java
+++ FJP6/GT/L001/Main.java
@@ -31,11 +31,40 @@ public class Main{
         }
         return root;
     }
+    
     public static void display(Node node){
+        System.out.print(node.data+" -> ");
+        for(Node child : node.children){
+            System.out.print(child.data+" ");
+        }
+        System.out.println(".");
 
+        for(Node child : node.children){
+            display(child);
+        }
+    }
+
+    public static int size(Node node){
+        int res = 0;
+        for(Node child : node.children){
+            res += size(child);
+        }
+
+        return res+1;    
+    }
+
+    public static int max(Node node){
+        int res = Integer.MIN_VALUE;
+        for(Node child : node.children){
+            res = Math.max(res , max(child));
+        }
+        return Math.max(res,node.data);
     }
     public static void main(String args[]){
         Integer []arr = {10, 20, 50, null, 60, null, null, 30, 70, 90, null, 100, null, 110, null, null, null, 40, 80, null, null, null};
         Node root = construct(arr);
+        display(root);
+        int treeSize = size(root);
+        System.out.println(treeSize);
     }
 }
