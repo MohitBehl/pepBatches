55b61a646bf6f341ba2064666b3bf8517a9b171b
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-17 23:57:55 +0530

    Update Main.java

--- FJP6/GT/L002/Main.java
+++ FJP6/GT/L002/Main.java
@@ -55,6 +55,70 @@ public class Main{
         System.out.println("Node Post "+node.data);
     }
 
+    public static void levelOrder(Node root){
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+        while(queue.size() > 0){
+            Node rem = queue.remove();
+            System.out.print(rem.data+" ");
+            for(Node child : rem.children){
+                queue.add(child);
+            }
+        }
+        System.out.println(".");
+    }
+
+    public static void levelOrderLinewise(Node node){
+        Queue<Node> mq = new ArrayDeque<>();
+        Queue<Node> hq = new ArrayDeque<>();
+        mq.add(node);
+
+        while(mq.size() > 0){
+            Node rem = mq.remove();
+            System.out.print(rem.data+" ");
+
+            for(Node child : rem.children){
+                hq.add(child);
+            }
+
+            if(mq.size() == 0){
+                System.out.println();
+                Queue<Node> tq = mq;
+                mq = hq;
+                hq = tq;
+            }
+        }
+    }
+    public static boolean find(Node node, int data) {
+        if(node.data == data){
+        return true;
+        }
+
+        for(Node child : node.children){
+        boolean rrec = find(child,data);
+        if(rrec){
+            return true;
+        }
+        }
+
+        return false;
+    }
+    public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+        if(node.data == data){
+        ArrayList<Integer> base = new ArrayList<>();
+        base.add(data);
+        return base;
+        }
+
+        for(Node child : node.children){
+        ArrayList<Integer> rres = nodeToRootPath(child,data);
+        if(rres.size() > 0){
+            rres.add(node.data);
+            return rres;
+        }
+        }
+        return new ArrayList<>();
+    }
     public static void main(String args[]){
         Integer []arr = {10, 20, 50, null, 60, null, null, 30, 70, 90, null, 100, null, 110, null, null, null, 40, 80, null, null, null};
         Node root = construct(arr);
