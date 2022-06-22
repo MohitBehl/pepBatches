8bc7cff0df4210c4c7d6be842c17b59616b04701
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-22 22:10:35 +0530

    Update Main.java

--- FJP6/BT/L001/Main.java
+++ FJP6/BT/L001/Main.java
@@ -19,9 +19,30 @@ public class Main{
         Node root = new Node(arr[0]);
         Stack<Pair> st = new Stack<>();
         st.push(new Pair(root));
-
+        int idx = 1;
         while(st.size() > 0){
-            // logic
+            Pair top = st.peek();
+            if(top.state == 0){
+                Integer val = arr[idx];
+                if(val != null){
+                    Node child = new Node(val);
+                    top.node.left = child;
+                    st.push(new Pair(child));
+                }
+                top.state++;
+                idx++;
+            }else if(top.state == 1){
+                Integer val = arr[idx];
+                if(val != null){
+                    Node child = new Node(val);
+                    top.node.right = child;
+                    st.push(new Pair(child));
+                }
+                top.state++;
+                idx++;
+            }else if(top.state == 2){
+                st.pop();
+            }
         }
         return root;
     }
