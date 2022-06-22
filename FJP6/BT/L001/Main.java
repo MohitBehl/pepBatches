a3e515e69c57daa4bb65ef15dbbde514b022d846
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-22 22:32:27 +0530

    code

--- FJP6/BT/L001/Main.java
+++ FJP6/BT/L001/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Main{
     public static class Node{
         int data;
@@ -46,7 +47,18 @@ public class Main{
         }
         return root;
     }
-    public static void main(String args[]){
+    public static void display(Node node){
+        if(node == null)return;
+        System.out.print(node.left == null ? "." : node.left.data);
+        System.out.print(" <- "+node.data+" -> ");
+        System.out.println(node.right == null ? "." : node.right.data);
 
+        display(node.left);
+        display(node.right);        
+    }
+    public static void main(String args[]){
+        Integer arr[] = {10,20,40,60,null,null,null,50,70,null,null,80,null,null,30,null,90,100,null,null,null};
+        Node root = construct(arr);
+        display(root);
     }
 }
