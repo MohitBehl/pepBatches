e47d2e542433fc45c4c9aceab5c062a62d9bb002
MohitBehl <mohitbehlofficial@gmail.com>
2022-06-28 22:25:58 +0530

    code

--- DSA March & FJP-7/8. Generic Tree/L001/Main.java
+++ DSA March & FJP-7/8. Generic Tree/L001/Main.java
@@ -1,5 +1,6 @@
+import java.util.*;
 public class Main {
-    private class Node{
+    private static class Node{
         int data;
         ArrayList<Node> children;
         Node(int val){
@@ -8,10 +9,38 @@ public class Main {
         }
     }
     public static Node constructGT(Integer arr[]){
-        
+        Stack<Node> st = new Stack<>();
+        Node root = new Node(arr[0]);
+        st.push(root);
+        int idx = 1;
+        while(idx < arr.length){
+            Integer vl = arr[idx];
+            if(vl == null){
+                st.pop();
+            }else{
+                Node node = new Node(vl);
+                Node par = st.peek();
+                par.children.add(node); // link
+                st.push(node);
+            }
+            idx++;
+        }
+        return root;
+    }
+    public static void display(Node node){
+        System.out.print(node.data+" -> ");
+        for(Node child : node.children){
+            System.out.print(child.data+" ");
+        }
+        System.out.println();
+
+        for(Node child : node.children){
+            display(child);
+        }
     }
     public static void main(String args[]){
         Integer arr[] = {10,20,null,30,50,null,60,null,null,40,null,null};
         Node root = constructGT(arr);
+        display(root);
     }
 }
