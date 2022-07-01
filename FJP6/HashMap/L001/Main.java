deb6247916cb7373133a4ed06d3633491d9b334e
MohitBehl <mohitbehlofficial@gmail.com>
2022-07-01 23:10:59 +0530

    code

--- FJP6/HashMap/L001/Main.java
+++ FJP6/HashMap/L001/Main.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
+import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.Collections;
 
 public class Main{
-    public static void main(String args[]){
+    public static void HMIntro(){
         HashMap<String,Integer> hm = new HashMap<>();
         System.out.println(hm);
         // add an ele
@@ -26,5 +29,57 @@ public class Main{
         for(String key : hm.keySet()){
             System.out.println("key : "+key +" val : "+hm.get(key));
         }
+    }
+    public static void PQIntro(){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        System.out.println(pq);
+        pq.add(10);
+        // System.out.println(pq);
+        pq.add(-1);
+        // System.out.println(pq);
+        pq.add(2);
+        // System.out.println(pq);
+        pq.add(15);
+        // System.out.println(pq);
+        pq.add(3);
+        // System.out.println(pq);
+        pq.add(6);
+        // System.out.println(pq);
+        pq.add(4);
+        System.out.println(pq);
+        System.out.println(pq.peek());
+        System.out.println(pq.size());
+
+        while(pq.size() > 0){
+            System.out.print(pq.remove()+" ");
+        }
+    }
+    public static char maxFreqCh(String str){
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int idx = 0 ; idx < str.length() ; idx++){
+            char ch = str.charAt(idx);
+            if(hm.containsKey(ch)){
+                hm.put(ch,hm.get(ch)+1); //  update
+            }else{
+                hm.put(ch, 1); // add
+            }
+        }
+
+        int maxFreq = 0;
+        char mfch = ' ';
+        for(Character ch : hm.keySet()){
+            if(maxFreq < hm.get(ch)){
+                maxFreq = hm.get(ch);
+                mfch = ch;
+            }
+        }
+        return mfch;
+    }
+    public static void main(String args[]){
+        // Scanner scn = new Scanner(System.in);
+        // String str = scn.nextLine();
+        // char ch = maxFreqCh(str);
+        // System.out.println(ch);
+        
     }
 }
