d75f0efa9558150ce2707076223a38744abc8dcd
MohitBehl <mohitbehlofficial@gmail.com>
2022-07-09 21:53:02 +0530

    Create Main.java

---
+++ DSA March & FJP-7/9. BT/L001/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public class Node{
+        // fill it
+    }
+    public static Node construct(Integer arr[]){
+        // fill it
+    }
+    public static void display(Node node){
+        // fill it
+    }
+    public static void main(String args[]){
+        Integer arr[] = {10,20,40,null,null,50,70,null,null,null,30,null,60,null,null};
+        Node root = construct(arr);
+        display(root);
+    }
+}
